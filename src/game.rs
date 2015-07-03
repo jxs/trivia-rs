5055cb2b45093e3d9f7fc678132d7085e6b60829
João Oliveira <hello@jxs.pt>
2015-07-03 01:51:52 +0100

    add tests

--- src/game.rs
+++ src/game.rs
@@ -46,7 +46,7 @@ impl Game {
         }
     }
 }
-#[derive (Clone)]
+#[derive (Clone, Debug, PartialEq)]
 struct Question {
     pub title: String,
     pub answer: String,
@@ -71,7 +71,7 @@ impl Question {
                           .ok_or("failed to parse jservice.io random question json field answer"));
 
         let answer = try!(answer.as_string().
-                         ok_or("failed to parse jservice.io random question json field answer"))
+                          ok_or("failed to parse jservice.io random question json field answer"))
             .to_string();
 
         let id = try!(data.get("id")
@@ -79,9 +79,9 @@ impl Question {
         let id= try!(id.as_u64().
                      ok_or("failed to parse jservice.io random question json field id"));
         let value = try!(data.get("value")
-                      .ok_or("failed to parse jservice.io random question json field value"));
+                         .ok_or("failed to parse jservice.io random question json field value"));
         let value= try!(value.as_u64().
-                     ok_or("failed to parse jservice.io random question json field value"));
+                        ok_or("failed to parse jservice.io random question json field value"));
 
         Ok(Question{
             title: title,
@@ -127,5 +127,67 @@ impl From<&'static str> for GameError {
 
     fn from(err: &'static str) -> GameError {
         GameError::QuestionParserError(err)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::Question;
+    use std::io;
+    use std::io::Read;
+    use hyper;
+    use hyper::Client;
+    use hyper::header::Connection;
+    use rustc_serialize::json::{ParserError, Json};
+
+    #[test]
+    fn test_can_generate_new_question() {
+        let mut game = Game::new();
+        let question = game.new_question();
+        question.unwrap();
+    }
+
+
+    #[test]
+    fn test_can_parse_json() {
+        let mut game = Game::new();
+        let json_string: &str = r#"[{"id":95779,"answer":"1","question":"An NFL game ended 17-7; the losing team scored no touchdowns but this many field goals","value":600,"airdate":"2010-06-03T12:00:00.000Z","created_at":"2014-02-14T02:01:25.720Z","updated_at":"2014-02-14T02:01:25.720Z","category_id":12744,"game_id":null,"invalid_count":null,"category":{"id":12744,"title":"let's be logical","created_at":"2014-02-14T02:01:25.025Z","updated_at":"2014-02-14T02:01:25.025Z","clues_count":5}}]"#;
+        let json = Json::from_str(json_string).unwrap();
+        let question = Question::from_json(json).unwrap();
+        let question2 = Question{
+            id: 95779,
+            title: "An NFL game ended 17-7; the losing team scored no touchdowns but this many field goals".to_string(),
+            answer: "1".to_string(),
+            value: 600
+        };
+        assert_eq!(question, question2);
+    }
+
+    #[test]
+    fn expect_array_json() {
+        let mut game = Game::new();
+        let json_string: &str = r#"{"id":95779,"answer":"1","question":"An NFL game ended 17-7; the losing team scored no touchdowns but this many field goals","value":600,"airdate":"2010-06-03T12:00:00.000Z","created_at":"2014-02-14T02:01:25.720Z","updated_at":"2014-02-14T02:01:25.720Z","category_id":12744,"game_id":null,"invalid_count":null,"category":{"id":12744,"title":"let's be logical","created_at":"2014-02-14T02:01:25.025Z","updated_at":"2014-02-14T02:01:25.025Z","clues_count":5}}"#;
+        let json = Json::from_str(json_string).unwrap();
+        let question = Question::from_json(json);
+        match question {
+            Err(GameError::QuestionParserError(msg)) => assert_eq!(msg, "failed to parse jservice.io random question json, not an array"),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn expect_object_json() {
+        "failed to parse jservice.io random question json, not an object";
+        let mut game = Game::new();
+        let json_string: &str = r#"[[{"id":95779,"answer":"1","question":"An NFL game ended 17-7; the losing team scored no touchdowns but this many field goals","value":600,"airdate":"2010-06-03T12:00:00.000Z","created_at":"2014-02-14T02:01:25.720Z","updated_at":"2014-02-14T02:01:25.720Z","category_id":12744,"game_id":null,"invalid_count":null,"category":{"id":12744,"title":"let's be logical","created_at":"2014-02-14T02:01:25.025Z","updated_at":"2014-02-14T02:01:25.025Z","clues_count":5}}]]"#;
+        let json = Json::from_str(json_string).unwrap();
+        let question = Question::from_json(json);
+        match question {
+            Err(GameError::QuestionParserError(msg)) => assert_eq!(msg, "failed to parse jservice.io random question json, not an object"),
+            _ => assert!(false)
+        }
+
+
     }
 }
